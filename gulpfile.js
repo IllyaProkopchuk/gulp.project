7b880332262ce0b308a6b20305a6570d967b5ac6
Sirax19 <prokop4uk19@gmail.com>
2019-07-25 17:42:20 +0300

    base gulp-project done 0.0.1

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,49 @@ const sass = require('gulp-sass');
 const cssnano = require('gulp-cssnano');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const image = require('gulp-image');
+const browsersync = require('browser-sync').create();
 
-function styles(){
-    return gulp.src('app/styles/**/*.sccs')
-        .pipe(sass().on('error',sass.logError))
+
+const paths = {
+    html: {
+        src: 'app/**/*.html',
+        dest: 'build/'
+    },
+    styles: {
+        src: 'app/styles/**/*.scss',
+        dest: 'build/css'
+    },
+    scripts: {
+        src: 'app/js/**/*.js',
+        dest: 'build/scripts'
+    },
+    images: {
+        src: 'app/images/*.*',
+        dest: 'build/images'
+    }
+}
+
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: './build'
+        },
+        port: 3000
+    });
+    done();
+}
+
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+function styles() {
+    return gulp.src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
         .pipe(cssnano())
         .pipe(autoprefixer())
         .pipe(rename(
@@ -14,30 +53,44 @@ function styles(){
                 suffix: '.min'
             }
         ))
-        .pipe(gulp.dest('build/css'))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browsersync.stream())
 }
 
-function html(){
-    return gulp.src('app/**/*.html')
-        .pipe(gulp.dest('build/'))
+function html() {
+    return gulp.src(paths.html.src)
+        .pipe(gulp.dest(paths.html.dest))
+        .pipe(browsersync.stream())
 }
 
-function watch(){
-    gulp.watch('app/styles/**/*.sccs', styles);
-    gulp.watch('app/**/*.html', html);
+function scripts() {
+    return gulp.src(paths.scripts.src)
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.scripts.dest))
+        .pipe(browsersync.stream())
 }
 
-const build = gulp.parallel(styles, html);
+function images() {
+    return gulp.src(paths.images.src)
+        .pipe(image())
+        .pipe(gulp.dest(paths.images.dest))
+        .pipe(browsersync.stream())
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, html);
+    gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.scripts.src, scripts);
+    gulp.watch(paths.images.src, images);
+    gulp.watch('./app/*.html', gulp.series(browserSyncReload));
+}
+
+const build = gulp.parallel(styles, html, scripts, images);
 
 gulp.task('build', build);
 
-gulp.task('default', gulp.parallel(watch, build))
-
-
-
-
-
-
+gulp.task('default', gulp.parallel(watch, build, browserSync))
 
 
 
